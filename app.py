aa1bd5fd646002be64e64248372c658b613b16d5
Lucas <lucas.hartridge9@gmail.com>
2022-01-20 16:22:14 +0100

    Adding env variable for postgrees

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-
+import os
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -16,7 +16,7 @@ def create_app(test: bool):
     app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
     app.secret_key = 'eminem'
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_2', 'sqlite:///data.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['PROPAGATE_EXCEPTIONS'] = True
 
